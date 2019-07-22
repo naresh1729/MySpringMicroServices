b49e61fcbfec71efc431dbd0e98c721007e9376c
naresh1729 <nareshkumardangeti95@gmail.com>
2019-07-22 07:39:17 +0530

    Currency Exchange Service

---
+++ currency-exchange-service/src/main/java/com/naresh/microservices/currencyexchangeservice/beans/ExchangeValue.java
@@ -0,0 +1,61 @@
+package com.naresh.microservices.currencyexchangeservice.beans;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ExchangeValue {
+
+	@Id
+	private long id;
+
+	@Column(name = "currency_from")
+	private String from;
+
+	@Column(name = "currency_to")
+	private String to;
+
+	private BigDecimal conversionMultiple;
+	private int port;
+
+	public ExchangeValue() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExchangeValue(long id, String from, String to, BigDecimal conversionMultiple, int port) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+		this.port = port;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+}
